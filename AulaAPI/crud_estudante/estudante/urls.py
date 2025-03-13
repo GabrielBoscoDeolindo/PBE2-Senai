3d5352a4febfa169d8208bcdbdf69a3e540846a3
GabrielBoscoDeolindo <gabibosco01@gmail.com>
2025-03-13 16:28:58 -0300

    changes to urls.py

--- AulaAPI/crud_estudante/estudante/urls.py
+++ AulaAPI/crud_estudante/estudante/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('alunos/', views.listar_alunos),
-    path('alunos/<int:pk>', views.detalhe_aluno),
-    path('alunos/criar/', views.criar_aluno),
+    path('aluno/detalhes/<int:pk>', views.detalhe_aluno),
+    path('aluno/criar/', views.criar_aluno),
     path('aluno/alterar/<int:pk>', views.alterar_aluno),
     path('aluno/apagar/<int:pk>', views.deletar_informacoes),
     path('fazAlgo/<str:texto>', views.macharete),
