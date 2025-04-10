8b3314ba131ed8afe861f4697e2a3d8c381d1b53
GabrielBoscoDeolindo <gabibosco01@gmail.com>
2025-04-10 17:02:10 -0300

    classbased v2

--- classbased/pato/views.py
+++ classbased/pato/views.py
@@ -2,10 +2,27 @@ from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView
 from .models import Pato
 from .serializers import PatoSerializer
-# Create your views here.
+from rest_framework.pagination import PageNumberPagination
+from rest_framework import serializers
+
+class PatoPaginacao(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 10
 
 class PatoListCreateAPIView(ListCreateAPIView):
     queryset = Pato.objects.all()
     serializer_class = PatoSerializer
+    pagination_class = PatoPaginacao
 
-    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        id = self.request.query_params.get('id')
+        if id:
+            queryset = queryset.filter(id__icontains=id)
+        return queryset
+
+    def perform_create(self, serializer):
+        if serializer.validated_data['peso'] < 0:
+            raise serializers.ValidationError("O peso não pode ser negativo")
+        serializer.save()
